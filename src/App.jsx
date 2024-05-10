4df2dc9c20f5458ebac31a2ab9582949f4e2e200
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-05-10 18:15:36 +0200

    structured App component

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Header,
+  WhatGPT3,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
